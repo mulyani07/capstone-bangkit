add8b66e5c1fdf91fa896e59c6f36cba1fae0fe2
mulyani07 <153817975+mulyani07@users.noreply.github.com>
2024-12-10 00:50:15 +0700

    Delete models directory

--- models/instruction.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const instructionSchema = new mongoose.Schema({
-  step: { type: Number, required: true },
-  description: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Instruction', instructionSchema);
