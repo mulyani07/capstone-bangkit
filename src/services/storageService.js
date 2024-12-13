62ef637c3b94c2f056db393f6361cfc227ceaf3d
mulyani07 <153817975+mulyani07@users.noreply.github.com>
2024-12-13 13:52:17 +0700

    Delete src directory

--- src/services/storageService.js
+++
@@ -1,8 +0,0 @@
-const tf = require('@tensorflow/tfjs-node');
-
-async function loadModelFromCloud() {
-    const modelUrl = process.env.CLOUD_MODEL_URL; // URL Cloud Storage
-    return await tf.loadLayersModel(modelUrl);
-}
-
-module.exports = { loadModelFromCloud };
